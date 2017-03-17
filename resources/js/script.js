c8b7e58c74f5a25367502256141cbd744020c79b
Ricardo <rick@reddraft.com>
2017-03-16 22:18:21 -0500

    Add typed.js and inmplemented in the header

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,6 +1,26 @@
 $(document).ready(function() {
+
+  /*******************************
+  **** FULL SCREEN NAVIGATION ****
+  ********************************/
+
   $('#toggle').click(function() {
     $(this).toggleClass('active');
     $('#menu-overlay').toggleClass('open');
   });
+
+
+  /*******************************
+  **** TYPED JS ****
+  ********************************/
+
+  $(function(){
+    $(".hero-text-box span").typed({
+        strings: [ " ^1000 creatives", "organizers", "doers", "idea makers"],
+        typeSpeed: 90,
+        loop: true
+    });
+  });
+
+
 });
